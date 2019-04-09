f7dcf895556770756c7d5e2d0902f41346afb223
minsikmoon <msmoon.business@gmail.com>
2019-04-10 00:32:19 +0900

    after before class

--- src/test/java/com/study/junit/helper/BeforeAfterTest.java
+++ src/test/java/com/study/junit/helper/BeforeAfterTest.java
@@ -1,29 +1,39 @@
 package com.study.junit.helper;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BeforeAfterTest {
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before Class==");
+	}
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("after Class==");
+	}
 
-    @Before
-    public void setup() {
-        System.out.println("Before Test");
-    }
+	@Before
+	public void setup() {
+		System.out.println("Before Test");
+	}
 
-    @Test
-    public void test() {
-        System.out.println("test1 executed");
-    }
+	@Test
+	public void test() {
+		System.out.println("test1 executed");
+	}
 
-    @Test
-    public void test2() {
-        System.out.println("test2 executed");
-    }
+	@Test
+	public void test2() {
+		System.out.println("test2 executed");
+	}
 
-    @After
-    public void tearddown() {
-        System.out.println("after test");
-    }
+	@After
+	public void tearddown() {
+		System.out.println("after test");
+	}
 
 }
